27749305438e7ad64e3dfdd5420d0f8056672977
jasonlazar <jasonlaz98@yahoo.gr>
2021-06-05 13:16:55 +0300

    Add dummy Move classes

---
+++ src/main/java/gr/ntua/vrp/tabu/Move.java
@@ -0,0 +1,5 @@
+package gr.ntua.vrp.tabu;
+
+public abstract class Move {
+	public abstract void applyMove(TabuSearchSolver s);
+}
---
+++ src/main/java/gr/ntua/vrp/tabu/SingleInsertionMove.java
@@ -0,0 +1,11 @@
+package gr.ntua.vrp.tabu;
+
+public class SingleInsertionMove extends Move {
+
+	@Override
+	public void applyMove(TabuSearchSolver s) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
