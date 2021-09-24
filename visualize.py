21a6a9890ff2c779368dff4ba7daf619d4c3e248
jasonlazar <jasonlaz98@yahoo.gr>
2021-09-24 20:48:50 +0300

    Fixed visualization

--- visualize.py
+++ visualize.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 """Visualize the routes that are contained in a file.
 
 The user must provide the path of the routes file as a command line argument.
@@ -31,31 +33,34 @@ with open(sys.argv[1], "r") as routeFile:
             line = instanceFile.readline().strip()
 
     fig, ax = plt.subplots()
-    line = routeFile.readline().strip()
+    line = routeFile.readline()
     i = -1
     while line:
+        line = line.strip()
         if line.startswith("Vehicle"):
             i += 1
             route = line.split(":")[1].strip()
             route = list(map(int, route.split("->")))
-            verts = [coords[i] for i in route]
-            codes = [Path.LINETO for i in route]
+            # verts = [coords[x] for x in route]
+            verts = [coords[x-1] for x in route]
+            codes = [Path.LINETO for _ in route]
             codes[0] = Path.MOVETO
             codes[-1] = Path.CLOSEPOLY
             path = Path(verts, codes)
-            patch = patches.PathPatch(path, facecolor=colours[i%len(colours)], lw=2)
+            patch = patches.PathPatch(path, edgecolor=colours[i%len(colours)], facecolor='none', lw=2)
             ax.add_patch(patch)
-        line = routeFile.readline().strip()
+        line = routeFile.readline()
 
     Xs = [x[0] for x in coords]
     Ys = [x[1] for x in coords]
-    min_x = min(Xs)
-    max_x = max(Xs)
-    min_y = min(Ys)
-    max_y = max(Ys)
+    min_x = min(Xs) * 0.95
+    max_x = max(Xs) * 1.05
+    min_y = min(Ys) * 0.95
+    max_y = max(Ys) * 1.05
 
     ax.set_xlim(min_x, max_x)
     ax.set_ylim(min_y, max_y)
+    ax.grid(True)
     
     datasets_index = instanceName.find("datasets")
     if datasets_index != -1:
